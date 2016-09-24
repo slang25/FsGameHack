8f1a3811a0a69dff2db7fce0d7e2e91bdaf66088
corwin.tomkinson <corwin.tomkinson@axonn.co.uk>
2016-09-24 16:21:07 +0100

    fixes

--- canvas.fsx
+++ canvas.fsx
@@ -18,9 +18,9 @@ canvas.height <- 648.
 // ctx.fillStyle <- U3.Case1 "rgba(0, 0, 200, 0.5)"
 // ctx.fillRect (30., 30., 55., 50.)
 
-let drawIntroText () =
+let drawIntroText state =
   let ctx = canvas.getContext_2d()
-  ctx.font <- "48px PixelFJVerdana12pt"
+  ctx.font <- "PixelFJVerdana12pt"
   ctx.fillStyle <- U3.Case1 "black"
   ctx.textBaseline <- "top"
   ctx.fillText ("Yo, sup?", 0., 270.)
@@ -32,28 +32,32 @@ type CatState =
     | None
 
 type GameScene = 
-     | IntoBlurb
+     | IntroBlurb
      | Main
 
 type GameState = { Scene: GameScene; Cat: CatState}
 
-let drawBackground () =
+let drawBackground state =
   let ctx = canvas.getContext_2d ()
   let backdropImg = (document.getElementById "BackdropImage") :?> HTMLImageElement
   ctx.drawImage (U3.Case1 backdropImg, 0., 0., 768., 648.)
 
-drawBackground ()
-//drawIntroText()
+
+let drawCanvas state =
+    canvas.width <- canvas.width
+    match state.Scene with
+    | GameScene.IntroBlurb -> drawIntroText(state)
+    | GameScene.Main -> drawBackground(state)
 
 let rec game () = async {
-    return! update { Scene = GameScene.IntoBlurb; Cat = CatState.None}
+    return! update { Scene = GameScene.IntroBlurb; Cat = CatState.None}
     ()}
 
 and update state = async{
-    match state.Scene with
-    | GameScene.IntoBlurb -> drawIntroText()
-    | GameScene.Main -> drawBackground()
+    drawCanvas state
     do! Async.Sleep(int (1000. / 60.))
     return! update state
     ()}
 // let drawSomething () = ctx.drawImage  
+
+game();
